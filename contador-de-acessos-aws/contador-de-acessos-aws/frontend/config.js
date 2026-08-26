58737ebcfb56620a7f78b415a9e20a327c689e9d
beatrisAS <23205680@aluno.univesp.br>
2026-08-26 01:41:22 -0300

    📝

--- contador-de-acessos-aws/contador-de-acessos-aws/frontend/config.js
+++
@@ -1,3 +0,0 @@
-// Cole aqui a URL exibida pelo cdklocal deploy, terminando em /acessos.
-// Exemplo: http://localhost:4566/restapis/abc123/local/_user_request_/acessos
-window.CONTADOR_API_URL = "";
